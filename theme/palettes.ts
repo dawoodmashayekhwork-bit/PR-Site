a1b322d9610f9b2c3bff05c983033cba8b521b42
Dawood <dawoodmashayekh.work@gmail.com>
2026-06-28 19:42:29 +0330

    Update palettes.ts

--- theme/palettes.ts
+++ theme/palettes.ts
@@ -370,7 +370,7 @@ export const colorPalettes = {
       darkgray: "#4A5568",
       dark: "#1A202C",
       secondary: "#007FFF",
-      tertiary: "#E500E5",
+      tertiary: "#e9ec2c",
       highlight: "rgba(0, 127, 255, 0.1)",
       textHighlight: "rgba(0, 127, 255, 0.25)",
     },
