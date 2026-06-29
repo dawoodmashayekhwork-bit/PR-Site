dc092b710de87607b5bfe235b1bb94dfaf81cdd3
Dawood <dawoodmashayekh.work@gmail.com>
2026-06-29 16:44:38 +0330

    Update Head.tsx

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -15,9 +15,7 @@ export default (() => {
     const titleSuffix = cfg.pageTitleSuffix ?? ""
     const title =
       (fileData.frontmatter?.title ?? i18n(cfg.locale).propertyDefaults.title) + titleSuffix
-    if (fileData.slug === "index") {
-	 return <></> }
-      const description =
+    const description =
       fileData.frontmatter?.socialDescription ??
       fileData.frontmatter?.description ??
       unescapeHTML(fileData.description?.trim() ?? i18n(cfg.locale).propertyDefaults.description)
