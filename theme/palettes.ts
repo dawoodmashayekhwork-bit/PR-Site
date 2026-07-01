c35b518346ec6075a1b9a5ae841e477a2d59e888
Dawood <dawoodmashayekh.work@gmail.com>
2026-07-01 14:14:34 +0330

    8

--- theme/palettes.ts
+++ theme/palettes.ts
@@ -370,7 +370,7 @@ export const colorPalettes = {
       darkgray: "#4A5568",
       dark: "#1A202C",
       secondary: "#032869",
-      tertiary: "#618500",
+      tertiary: "#B08968",
       highlight: "rgba(0, 127, 255, 0.1)",
       textHighlight: "rgba(0, 127, 255, 0.25)",
     },
